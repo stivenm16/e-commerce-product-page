bc2dca0477885b5279acb941daca80d6a3fb6a8c
Kevin <stiivenmendez16@gmail.com>
2022-05-23 20:38:17 -0500

    Add and rmv buttons functionalities

---
+++ index.js
@@ -0,0 +1,20 @@
+const btnAdd = document.querySelector('.btn-add')
+const btnRmv = document.querySelector('.btn-rmv')
+let quantity = document.querySelector('.quantity')
+
+let counter = 0
+function addItem() {
+    counter++
+    quantity.textContent = counter
+}
+
+function rmvItem() {
+    if (counter <= 0) {
+        return
+    } else {
+        counter--
+    }
+    quantity.textContent = counter
+}
+btnAdd.addEventListener('click', addItem)
+btnRmv.addEventListener('click', rmvItem)
