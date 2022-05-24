7e498d94f1200a6db03ad08ce843522b6b1e8363
Kevin <stiivenmendez16@gmail.com>
2022-05-24 10:28:50 -0500

    Selected and Unselected small Image

--- src/setImage.js
+++ src/setImage.js
@@ -1,18 +1,40 @@
 const largeImg = document.querySelector('.largeOne')
+const containerSmall = [...document.querySelectorAll('.card-small-image')]
+const smallSelected = [...document.querySelectorAll('.small')]
 const small1 = document.getElementById('1')
 const small2 = document.getElementById('2')
 const small3 = document.getElementById('3')
 const small4 = document.getElementById('4')
-let idClicked 
-
+let idClicked = 4
+smallSelected.src = `./images/image-product-${idClicked}-thumbnail.jpg`
 
 function setImage() { 
     largeImg.src = `./images/image-product-${idClicked}.jpg`
 }
+
+function selectSmallOne() {
+    
+    containerSmall[idClicked - 1].classList.toggle('card-small-image-selected')
+    smallSelected[idClicked - 1].classList.toggle('small-selected')
+    
+}
+
+function unSelectedSmall() {
+    for (let i = 0; i < containerSmall.length; i++) {
+        if (containerSmall[i].classList.contains('card-small-image-selected')){
+            containerSmall[i].classList.toggle('card-small-image-selected')
+            smallSelected[i].classList.toggle('small-selected')
+        }
+    }
+}
+
 function changeId(i) {
     idClicked = this.id
+    unSelectedSmall()
+    selectSmallOne()
     setImage()
 }
+
 
 small1.addEventListener('click', changeId)
 small2.addEventListener('click', changeId)
