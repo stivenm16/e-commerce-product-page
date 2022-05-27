8e61c1e3fe6694289d12da7e6df0fd439a0f316f
Kevin <stiivenmendez16@gmail.com>
2022-05-26 21:04:20 -0500

    checkout modal done

--- src/quantity.js
+++ src/quantity.js
@@ -1,20 +1,36 @@
+const deleteItem = document.querySelector('.cart-delete')
+let quantityCart = document.querySelector('.cart-quantity')
+let total = document.querySelector('.cart-total')
 const btnAdd = document.querySelector('.btn-add')
 const btnRmv = document.querySelector('.btn-rmv')
 let quantity = document.querySelector('.quantity')
 
+export let price = '125.00'
 export let counter = 0
+quantityCart.textContent = `$ ${price} x ${counter}`
+
 export function addItem() {
     counter++
+    let totalPrice = parseInt(price) * counter
     quantity.textContent = counter
-}
+    quantityCart.textContent = `$ ${price} x ${counter}`
+    total.textContent = ` $ ${totalPrice}`
 
+}
 export function rmvItem() {
     if (counter <= 0) {
         return
     } else {
         counter--
+        let totalPrice = parseInt(price) * counter
+        quantityCart.textContent = `$ ${price} x ${counter}`
+        total.textContent = ` $ ${totalPrice}`
     }
     quantity.textContent = counter
 }
+
 btnAdd.addEventListener('click', addItem)
 btnRmv.addEventListener('click', rmvItem)
+deleteItem.addEventListener('click', rmvItem)
+
+
